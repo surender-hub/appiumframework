bbaca7d22be18e02854e88cfc91c37d76ba7530d
Harish.x.Singh <harishsingh8646@gmail.com>
2025-03-27 17:17:32 +0530

    New Test Cases added in MyTrip......

--- src/main/java/pages/MyTripsPage.java
+++ src/main/java/pages/MyTripsPage.java
@@ -1,10 +1,147 @@
 package pages;
 
+import constant.ConstantClass;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import utils.ElementUtils;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyTripsPage {
 
+    public AndroidDriver driver;
+
+    public MyTripsPage(AndroidDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@text='My Trips']")
+    public WebElement myTripsMenu;
+
+    @FindBy(xpath = "//android.widget.EditText[@text='PNR / Booking Reference']")
+    public WebElement pnrField;
+
+    @FindBy(xpath = "//android.widget.EditText[@text='Last Name']")
+    public WebElement lastName;
+
+    @FindBy(xpath = "//android.widget.TextView[@text='Get Started']")
+    public WebElement getStartedBtn;
+
+    @FindBy(xpath = "(//android.widget.TextView[@text='Booking confirmed']/ancestor::*[position()=1]/following-sibling::android.view.ViewGroup//android.widget.TextView)[4]")
+    public WebElement bookingPnrAndTime;
+
+    @FindBy(xpath = "(//android.widget.TextView[contains(@text,'PNR')])[1]")
+    public WebElement allPNRValue;
+
+    @FindBy(xpath = "//android.widget.TextView[starts-with(@text,'Hello')]")
+    public WebElement guestFullName;
+
+    @FindBy(xpath = "(//android.widget.TextView[starts-with(@text,'PNR:')])[1]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup/android.widget.TextView")
+    public WebElement confirmedText;
+
+    @FindBy(xpath = "(//android.widget.TextView[@text='Booking confirmed']/ancestor::*[position()=1]/following-sibling::android.view.ViewGroup//android.widget.TextView)[1]")
+    public WebElement bookingDepartureCity;
+
+    @FindBy(xpath = "(//android.widget.TextView[@text='Booking confirmed']/ancestor::*[position()=1]/following-sibling::android.view.ViewGroup//android.widget.TextView)[3]")
+    public WebElement bookingDestinationCity;
+
+    @FindBy(xpath = "//android.widget.HorizontalScrollView//android.widget.TextView[1]")
+    public WebElement departure_City;
+
+    @FindBy(xpath = "//android.widget.HorizontalScrollView//android.widget.TextView[2]")
+    public WebElement destination_City;
+
+    @FindBy(xpath = "(//android.widget.TextView[@text='Departure Flight']/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup//android.widget.TextView)[1]")
+    public WebElement departure_Date;
+
+    @FindBy(xpath = "//android.widget.TextView[@text='Pax']/preceding-sibling::android.widget.TextView")
+    public WebElement bookingPassengerNum;
+
+    @FindBy(xpath = "//android.widget.TextView[@text='Pax']/preceding-sibling::android.widget.TextView")
+    public WebElement passenger_num;
+
+    @FindBy(xpath = "(//android.widget.TextView[@text=\"Departure Flight\"]/parent::android.view.ViewGroup/following-sibling::android.view.ViewGroup//android.widget.TextView)[5]")
+    public WebElement journey_time;
 
 
+    public void enterPNRinMyTripsMenu(String pnr_Value, String last_Name) {
+        ElementUtils.waitAndClickElement(myTripsMenu, ConstantClass.SHORT_WAIT_2);
+        pnrField.sendKeys(pnr_Value);
+        lastName.sendKeys(last_Name);
+        getStartedBtn.click();
+    }
+
+    public List<String> getBookingDetails() throws InterruptedException {
+
+        List<String> bookingDetails = new ArrayList<>();
+
+        ElementUtils.waitForElementVisible(By.xpath("//android.widget.TextView[@text=\"Quick actions\"]"), ConstantClass.LONG_WAIT_180);
+
+        String departure_City = bookingDepartureCity.getText();
+        String destination_City = bookingDestinationCity.getText();
+
+        String[] pnrDateTime = bookingPnrAndTime.getText().split("\\|");
+        String pnrDate = pnrDateTime[0].trim();
+        String journeyTime = pnrDateTime[1].trim();
+        String pnrNumber = pnrDateTime[2].trim();
+
+        bookingDetails.add(pnrNumber);
+        bookingDetails.add(departure_City);
+        bookingDetails.add(destination_City);
+        bookingDetails.add(pnrDate);
+        bookingDetails.add(journeyTime);
+
+        ElementUtils.scrollToEndAndCheck("Flight Details");
+
+        String passengerNumber = bookingPassengerNum.getText();
+
+        bookingDetails.add(passengerNumber);
+
+        driver.navigate().back();
+        return bookingDetails;
+    }
+
+
+    public void verifyPNRDetails(String pnr_Value) {
+        String pnrNum = allPNRValue.getText().replaceAll(".*\\s([A-Z0-9]+)$", "$1");
+        Assert.assertEquals(pnr_Value, pnrNum);
+    }
+
+    public void guestUserName() {
+//        String userFirstName = guestFullName.getText().replaceAll()
+    }
+
+    public void checkBookingConfirmed() {
+        Assert.assertEquals("Confirmed", confirmedText.getText());
+    }
+
+    public void checkDepartureCity(String expectedCity) {
+        String actualCity = departure_City.getText();
+        Assert.assertEquals(expectedCity, actualCity);
+    }
+
+    public void checkDestinationCity(String expectedCity) {
+        String actualCity = destination_City.getText();
+        Assert.assertEquals(expectedCity, actualCity);
+    }
+
+    public void checkBookingDate(String expectedDate) {
+        String actualDate = departure_Date.getText();
+        Assert.assertEquals(expectedDate, actualDate);
+    }
+
+    public void checkJourneyTime(String expectedJourneyTime) {
+        Assert.assertEquals(expectedJourneyTime, journey_time.getText());
+    }
+
+    public void checkPassengerCount(String expectedPassenger) {
+        Assert.assertEquals(expectedPassenger, passenger_num.getText());
+    }
 }
--- src/main/java/tests/MyTripsTest.java
+++ src/main/java/tests/MyTripsTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import base.BaseSuper;
 import base.BaseTest;
 import base.BaseTestLoginUser;
 import constant.ConstantClass;
@@ -12,7 +13,9 @@ import utils.ConfigUatReader;
 import utils.ElementUtils;
 import utils.LogUtils;
 
-public class MyTripsTest extends BaseTestLoginUser {
+import java.util.List;
+
+public class MyTripsTest extends BaseSuper {
 
     private WelcomePage welcomePage;
     private SearchPage searchPage;
@@ -23,29 +26,19 @@ public class MyTripsTest extends BaseTestLoginUser {
     private ValidFarePage validFarePage;
     private ElementUtils elementUtils;
     private ThreadWaitClass threadWaitClass;
+    private MyTripsPage myTripsPage;
     String currentPackage;
+    List<String> userBookingDetails;
+
 
     @BeforeClass
-    public void setup() {
+    public void setup() throws InterruptedException {
         welcomePage = new WelcomePage(driver);
         searchPage = new SearchPage(driver);
         validFarePage = new ValidFarePage(driver);
-    }
+        myTripsPage = new MyTripsPage(driver);
 
-    /*
-
-    @Test(priority = 1, description = "TC_001 - Verify the Guest user generate PNR")
-    @Description("Verify GuestUser booking ticket end-to-end flow")
-    @Severity(SeverityLevel.CRITICAL)
-    @Feature("GuestUser Booking Ticket Flow")
-    @Story("User should be able to Book Ticket and Generate Pnr")
-    @Step("{SearchPage.pnrDetails5}")
-    public void generatePnrOneWay() throws InterruptedException {
         LogUtils.info("Guest User booking ticket using skip to payment");
-
-        //welcomePage.loginByOtp();
-        welcomePage.clickLoginAsQuest();
-        LogUtils.info("User Enter Login Details For OTP");
         searchPage.clickOnTo();
         LogUtils.info("User Click on Going To Flight Link");
         searchPage.searchPlace(ConfigUatReader.getProperty("source"));
@@ -76,71 +69,64 @@ public class MyTripsTest extends BaseTestLoginUser {
         LogUtils.info("User Click on Pay Button");
         searchPage.clickOnButtonResponse();
         LogUtils.info("User Click on Response Button");
-//        searchPage.getPnrDetails();
-//        elementUtils.scrollToElementByText("Departure Flight");
+        userBookingDetails = myTripsPage.getBookingDetails();
+        myTripsPage.enterPNRinMyTripsMenu(userBookingDetails.get(0),ConfigUatReader.getProperty("lastname"));
 
-        searchPage.getPNRText();
-
-//        elementUtils.attachTextToAllureReport("PNR Details", "Generated PNR: " + SearchPage.pnrDetails5);
-//        Allure.step("Generated PNR from Allure.Step: " + SearchPage.pnrDetails5);
-//        LogUtils.info("PNR Details Generated");
     }
 
 
-     */
-
-
-    @Test(priority = 1, description = "TC_001 - Verify the Guest user generate PNR")
-    @Description("Verify GuestUser booking ticket end-to-end flow")
+    @Test(priority = 1, description = "TC_001 - Verify the Guest user generated PNR")
+    @Description("Verify GuestUser booking ticket PNR number")
     @Severity(SeverityLevel.CRITICAL)
     @Feature("GuestUser Booking Ticket Flow")
-    @Story("User should be able to Book Ticket and Generate Pnr")
-    @Step("{SearchPage.pnrDetails5}")
-    public void generatePnrOneWay() throws InterruptedException {
-        LogUtils.info("Guest User booking ticket using skip to payment");
-
-        //welcomePage.loginByOtp();
-        welcomePage.clickLoginAsQuest();
-        LogUtils.info("User Enter Login Details For OTP");
-        searchPage.clickOnTo();
-        LogUtils.info("User Click on Going To Flight Link");
-        searchPage.searchPlace(ConfigUatReader.getProperty("source"));
-        LogUtils.info("Search Destination city Mumbai");
-        searchPage.clickOnMumbaiFlight();
-        LogUtils.info("Select Destination city  first Flight ");
-        validFarePage.clickOnFutureDate(ConstantClass.FUTURE_DATE);
-        LogUtils.info("Select Future Date 3 Days Ahead ");
-        searchPage.clickOnSearchButton();
-        LogUtils.info("Click on Search Button");
-        searchPage.clickOnBookingList();
-        LogUtils.info("Select a  First Flight ");
-        // validFarePage.clickOnFlightReturn();
-        searchPage.bookingNextButton();
-        LogUtils.info("Click on next Button");
-        searchPage.enterUserDetails(ConfigUatReader.getProperty("firstname"), ConfigUatReader.getProperty("lastname"), ConfigUatReader.getProperty("dob"), ConfigUatReader.getProperty("phone"), ConfigUatReader.getProperty("email"));
-        LogUtils.info("Enter User Details");
-        searchPage.clickOnSkipToPayment();
-        searchPage.clickOnNetBanking();
-        LogUtils.info("User Click on Net banking Button");
-        searchPage.clickOnAddBank();
-        LogUtils.info("User Click on More Bank Button");
-        searchPage.searchAvenue(ConfigUatReader.getProperty("search_payment"));
-        LogUtils.info("Search Avenue payment method");
-        searchPage.clickOnAvenuePayment();
-        LogUtils.info("Select Avenue payment method");
-        searchPage.clickOnAvenueButtonPay();
-        LogUtils.info("User Click on Pay Button");
-        searchPage.clickOnButtonResponse();
-        LogUtils.info("User Click on Response Button");
-        searchPage.getPnrDetails();
-        elementUtils.scrollToElementByText("Departure Flight");
-
-       // searchPage.getPNRText();
-
-//        elementUtils.attachTextToAllureReport("PNR Details", "Generated PNR: " + SearchPage.pnrDetails5);
-//        Allure.step("Generated PNR from Allure.Step: " + SearchPage.pnrDetails5);
-//        LogUtils.info("PNR Details Generated");
+    @Story("User should be able to check details using PNR")
+    public void verifyPNRNumber() {
+        myTripsPage.verifyPNRDetails(userBookingDetails.get(0));
     }
+
+//    @Test
+    public void verifyGuestName() {
+        myTripsPage.guestUserName();
+    }
+
+    @Test(priority = 2)
+    public void verifyBookingConfirmed() {
+        myTripsPage.checkBookingConfirmed();
+
+    }
+
+    @Test(priority = 3)
+    public void verifyDepartureCity() {
+        myTripsPage.checkDepartureCity(userBookingDetails.get(1));
+    }
+
+    @Test(priority = 4)
+    public void verifyDestinationCity() {
+        myTripsPage.checkDestinationCity(userBookingDetails.get(2));
+    }
+
+    @Test(priority = 5)
+    public void verifyBookingDate() {
+        myTripsPage.checkBookingDate(userBookingDetails.get(3));
+    }
+
+    @Test(priority = 6)
+    public void verifyJourneyTime() {
+        System.out.println(userBookingDetails.get(4));
+       myTripsPage.checkJourneyTime(userBookingDetails.get(4));
+
+    }
+
+    @Test(priority = 7)
+    public void verifyPassengerCount() {
+        System.out.println(userBookingDetails.get(5));
+        myTripsPage.checkPassengerCount(userBookingDetails.get(5));
+    }
+
+
+
+
+
 
 
 
